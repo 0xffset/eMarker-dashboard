1c18feb10201aba7b7353bffaa74c3405055dde4
roleyder <roleyder02@gmail.com>
2020-08-12 17:04:36 -0400

    fix dirname

--- server/express.js
+++ server/express.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('../build'))
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.join('../build', 'index.html'))
+		res.sendFile(path.join(__dirname, '../build', 'index.html'))
 	})
 }
 
